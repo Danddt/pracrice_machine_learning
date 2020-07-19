0f6ac7c7af25449e6e5b2dcb94d51440dc5cd451
Danddt <dndnewton@gmail.com>
2020-07-19 10:48:18 +0300

    endpoints models

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -118,3 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework', # add django rest framework
+    # apps
+    'apps.endpoints'
+]
--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -19,3 +19,15 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+# backend/server/server/urls.py file
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.urls import path
+
+from apps.endpoints.urls import urlpatterns as endpoints_urlpatterns
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+urlpatterns += endpoints_urlpatterns
