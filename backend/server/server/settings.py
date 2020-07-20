3bc4b66520659049618eeeed6c4c193cf1d6bf07
Danddt <dndnewton@gmail.com>
2020-07-20 10:24:38 +0300

    add ml code

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -127,5 +127,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework', # add django rest framework
     # apps
-    'apps.endpoints'
+    'apps.endpoints',
+    'apps.ml'
 ]
